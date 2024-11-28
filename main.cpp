c2a8d6983a1baaf04107e677824946f390504608
Dyncais <dimiyriy@yandex.ru>
2024-11-28 20:33:46 +0300

    SomeCheck

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <enet/enet.h>
 #include "BattleTime.h"
+#include <string.h>
 
 enum class Statuses { clear, preset, block, fill }; 
 
@@ -18,6 +19,8 @@ enum class CellState {
 enum class SceneState{
     ChoosingMode,
     Preparation,
+    HostLobby,
+    JoinLobby,
     Playing
 };
 
@@ -37,6 +40,22 @@ struct GameFields
     {
     }
 };
+
+void FillGameField(std::map<std::pair<int, int>, Statuses>& Field, GameFields& Game)
+{
+    for(int i=0;i<10;i++)
+    {
+        for(int j=0;j<10;j++)
+            {
+                if (Field[{i,j}] == Statuses::fill)
+                {
+                    Game.PlayerField[10*i+j] = CellState::Ship;
+                }
+            }   
+    }
+    Game.EnemyField = Game.PlayerField;
+    return;
+}
 
 void AI_vs_Player(bool& turn, Vector2& mousePos, Rectangle& cellRect, int cellIndex, GameFields& Game)
 {
@@ -106,15 +125,116 @@ int main() {
         }
     }
 
-    SceneState current_state = SceneState::Preparation;
+    SceneState current_state = SceneState::ChoosingMode;
 
     bool player_turn{true};
 
     GameFields Game;
 
+
+     if (enet_initialize() != 0) {
+        std::cerr << "Не удалось инициализировать ENet!" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    ENetHost* server = nullptr;
+    ENetHost* client = nullptr;
+    ENetPeer* peer = nullptr;
+    GameFields fields;
+
+
     while (!WindowShouldClose()) {
+
         switch(current_state)
         {
+
+            case SceneState::ChoosingMode: {
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+
+            DrawText("Выберите режим:", 50, 50, 20, BLACK);
+            DrawText("1. Создать лобби (нажмите Enter)", 50, 100, 20, BLUE);
+            DrawText("2. Подключиться к лобби (нажмите Space)", 50, 150, 20, GREEN);
+
+            if (IsKeyPressed(KEY_ENTER)) {
+                ENetAddress address = {};
+                address.host = ENET_HOST_ANY;
+                address.port = 1234;          
+
+                server = enet_host_create(&address, 32, 2, 0, 0);
+                if (!server) {
+                    std::cerr << "Ошибка: сервер не создан!" << std::endl;
+                    break;
+                }
+                std::cout << "Сервер создан на порту 1234." << std::endl;
+                current_state = SceneState::HostLobby;
+
+            } else if (IsKeyPressed(KEY_SPACE)) {
+                
+                client = enet_host_create(nullptr, 1, 2, 0, 0);
+
+                if (!client) {
+                    std::cerr << "Ошибка: клиент не создан!" << std::endl;
+                    break;
+                }
+
+                ENetAddress address = {};
+                enet_address_set_host(&address, "localhost");
+	            address.port = 1234;
+                
+                peer = enet_host_connect(client, &address, 2, 0);
+                if (!peer) {
+                    std::cerr << "Ошибка: подключение к серверу не удалось!" << std::endl;
+                    break;
+                }
+                std::cout << "Клиент: попытка подключения к серверу." << std::endl;
+                current_state = SceneState::JoinLobby;
+            }
+                
+            
+
+            EndDrawing();
+            continue;
+        }
+            
+            
+            case SceneState::HostLobby: {
+                BeginDrawing();
+                ClearBackground(RAYWHITE);
+
+                DrawText("Ожидание подключения...", 50, 50, 20, BLACK);
+                
+
+                ENetEvent event = {};
+                if (enet_host_service(server, &event, 10) > 0) {
+                    if (event.type == ENET_EVENT_TYPE_CONNECT) {
+                        std::cout << "Сервер: клиент подключился." << std::endl;
+                        peer = event.peer;
+                        current_state = SceneState::Preparation;
+                    }
+                }
+                EndDrawing();
+                continue;
+            }
+
+             case SceneState::JoinLobby: {
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+
+            DrawText("Подключение к серверу...", 50, 50, 20, BLACK);
+
+            ENetEvent event = {};
+            if (enet_host_service(client, &event, 5000) > 0) { 
+                if (event.type == ENET_EVENT_TYPE_CONNECT) {      
+                    puts ("Connection to some.server.net:1234 succeeded.");             
+                    current_state = SceneState::Preparation;
+                }
+            }
+
+            EndDrawing();
+            continue;
+        }
+
             case SceneState::Preparation:
             {
                 Vector2 mousePos = GetMousePosition();
@@ -207,16 +327,7 @@ int main() {
                     {
                         if (CheckCollisionPointRec(mousePos, {100, 250, 50, 25}))
                         {
-                                for (int i = 0;i<10;i++)
-                                {
-                                    for (int j = 0;j<10;j++)
-                                    {
-                                        if (Field[{i,j}] == Statuses::fill)
-                                            Game.PlayerField[10*i+j] = CellState::Ship;
-                                    }
-                                }
-
-                                Game.EnemyField = Game.PlayerField;
+                            FillGameField(Field,Game);
                             current_state = SceneState::Playing;
                             EndDrawing();
                             continue;
@@ -228,7 +339,6 @@ int main() {
                 continue;
             }
             
-
             case SceneState::Playing:
             {
                 Vector2 mousePos = GetMousePosition();
@@ -239,7 +349,7 @@ int main() {
                 for (int x = 0; x < 10; x++) {
                     for (int y = 0; y < 10; y++) {
                         Rectangle cellRect = {20.0f * x, 20.0f * y, 19.0f, 19.0f};
-
+                        
                         int cellIndex = 10 * x + y;
 
                         switch (Game.PlayerField[cellIndex]) {
@@ -257,13 +367,9 @@ int main() {
                             break;
                         }
                         DrawRectangleLinesEx(cellRect, 1, BLACK);
-                    }
-                }
 
-                for (int x = 0; x < 10; x++) {
-                    for (int y = 0; y < 10; y++) {
-                        Rectangle cellRect = {500 + 20.0f * x, 20.0f * y, 19.0f, 19.0f};
-                        int cellIndex = 10 * x + y;
+
+                        cellRect = {500 + 20.0f * x, 20.0f * y, 19.0f, 19.0f};
 
                         switch (Game.EnemyField[cellIndex]) {
                         case CellState::Hidden: 
@@ -283,13 +389,19 @@ int main() {
                         DrawRectangleLinesEx(cellRect, 1, BLACK);
 
                         AI_vs_Player(player_turn, mousePos, cellRect, cellIndex, Game);
-                        
                     }
+
                 }
+
             EndDrawing();
             continue;
             }
+        
+            
         }
+    if (server) enet_host_destroy(server);
+    if (client) enet_host_destroy(client);
+    enet_deinitialize();
     return 0;
-}
+} 
 }
